c57a2516b26b00a23db7f0bf865457ee4477ef63
Felix Villalobos <felix.evl@gmail.com>
2019-04-01 12:25:43 -0500

    Customer changed input text

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -82,6 +82,7 @@ $(function() {
     .then(
       function() {
         console.log("Created new customer");
+        $("#customer").val("");
         $("#customer").attr("placeholder", "Customer changed");
       }
     );
